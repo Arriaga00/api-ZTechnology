fca327e7b2c3472c2e87a90e87e8474412a829a5
andres arriaga <andresfelipearriaga@gmail.com>
2024-04-29 17:52:48 -0500

    cambiando las variavles de entorno

--- db/conection.ts
+++ db/conection.ts
@@ -1,7 +1,11 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv'
+dotenv.config()
 
-const db = new Sequelize('ventapps', 'root', '', {
-    host: 'localhost',
+const env = process.env
+
+const db = new Sequelize(env.DB_NAME || 'ventapps', env.DB_USER || 'root', env.DB_PASSWORD||'', {
+    host: env.DB_HOST||'localhost',
     dialect: 'mysql'
   })
 
